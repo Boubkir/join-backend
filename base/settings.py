85fad7dd02cbad1a5c23ee64935042ad6474b332
Boubkir <109249622+Boubkir@users.noreply.github.com>
2023-07-18 13:28:58 +0200

    added enviroment variables

--- base/settings.py
+++ base/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,12 +135,13 @@ REST_FRAMEWORK = {
     ]
 }
 
+PASSWORD = config('PASSWORD')
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "boubkir.benamar@gmail.com"
-EMAIL_HOST_PASSWORD = "uehzslhfbicylkcw"
+EMAIL_HOST_PASSWORD = PASSWORD
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Join Team <boubkir.benamar@gmail.com>'
 
